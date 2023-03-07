52ced8bd44a3f077296a481fe0928972c7c24b51
serteri <serteri@hotmail.com>
2023-03-07 20:32:20 +1000

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AppComponent } from './app.component';
+import { initializeApp } from '@angular/fire/app';
+import { environment } from 'src/environments/environment.development';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TodoComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBjHWK-ecoJKkgvw-rRwNeMJVG1ZWXnRfU',
+    authDomain: 'todo-app-c8466.firebaseapp.com',
+    projectId: 'todo-app-c8466',
+    storageBucket: 'todo-app-c8466.appspot.com',
+    messagingSenderId: '964252110439',
+    appId: '1:964252110439:web:8af690eca7a14573788ec2',
+  },
+};
